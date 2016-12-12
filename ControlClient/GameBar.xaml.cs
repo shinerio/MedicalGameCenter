2f800d635c7c8ed3e16142ec1f2a3dac6ed178e7
shinerio <jstxzhangrui@163.com>
2016-12-12 20:36:49 +0800

    更新Newton.json库，shinerio,2016-12-12 20:36:47

--- ControlClient/GameBar.xaml.cs
+++ ControlClient/GameBar.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,14 +22,17 @@ namespace ControlClient
     public partial class GameBar : Window
     {
         private static GameBar gameBar;
+        private static float scaleSize = 1.25f;
+        public static   int nowColunm = 0;
+        public static   int nowRow = 0;
         private GameBar()
         {
             InitializeComponent();
             this.WindowStartupLocation = WindowStartupLocation.Manual;
             int SH = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;  
-            int SW = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Right;       
-            this.Left = 0;   //控件和屏幕分辨率值之间有差异
-            this.Top = 0;       
+            int SW = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Right;
+            this.Left = SW / scaleSize - this.Width;   //控件和屏幕分辨率值之间有差异
+            this.Top = (SH / scaleSize - this.Height) / 2;       
         }
 
         public static GameBar getInstance()
@@ -39,5 +43,56 @@ namespace ControlClient
             }
             return gameBar;
         }
+
+        public void InitGame()
+        {
+            string file = System.Windows.Forms.Application.ExecutablePath;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(file);
+            foreach (string key in config.AppSettings.Settings.AllKeys)
+            {
+                if (key.Contains("gamepath"))
+                {
+                    String path = config.AppSettings.Settings[key].Value.ToString();
+                    doUpdate(path);
+                }
+            }
+
+        }
+        [System.Runtime.InteropServices.DllImport("shell32.dll")]
+        private static extern int ExtractIconEx(string lpszFile, int niconIndex, IntPtr[] phiconLarge, IntPtr[] phiconSmall, int nIcons);
+        //更新游戏
+        public void doUpdate(String fileName)
+        {
+            IntPtr[] largeIcons, smallIcons;  //存放大/小图标的指针数组  
+            string appPath = @fileName;
+            //第一步：获取程序中的图标数         
+            int IconCount = ExtractIconEx(appPath, -1, null, null, 0);
+            //第二步：创建存放大/小图标的空间    
+            largeIcons = new IntPtr[IconCount];
+            smallIcons = new IntPtr[IconCount];
+            //第三步：抽取所有的大小图标保存到largeIcons和smallIcons中        
+            ExtractIconEx(appPath, 0, largeIcons, smallIcons, IconCount);
+            //  for (int i = 0; i < IconCount; i++)
+            //    {
+            BitmapSource returnSource = null;
+            if (IconCount > 0)
+            {
+                System.Drawing.Icon newIcon = System.Drawing.Icon.FromHandle(largeIcons[0]);
+                System.Drawing.Bitmap bmp = newIcon.ToBitmap();
+                returnSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(bmp.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            Image img = new Image();
+            if (returnSource == null)
+            {
+                img.Source = new BitmapImage(new Uri("./img/defaultGameICO.png",
+                                                   UriKind.Relative));
+            }
+            else
+            {
+                img.Source = returnSource;
+            }
+           
+        }
+
     }
 }
