5623103968b975e210fe1fd9546411daffdcc1f8
tomxie <tomxie1994@gmail.com>
2016-12-12 17:01:31 +0800

    merge, tomxie, 2016-12-12 17:01:28

--- ControlClient/GameBar.xaml.cs
+++ ControlClient/GameBar.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,13 +23,23 @@ namespace ControlClient
     public partial class GameBar : Window
     {
         private static GameBar gameBar;
+        private static float scalingFactor = GetScalingFactor();
         private GameBar()
         {
             InitializeComponent();
             this.WindowStartupLocation = WindowStartupLocation.Manual;
             int SH = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;  
-            int SW = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Right;       
-            this.Left = 0;   //控件和屏幕分辨率值之间有差异
+            int SW = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width;
+            double dpiX, dpiY;
+            PresentationSource presentationsource = PresentationSource.FromVisual(this);
+
+            if (presentationsource != null) // make sure it's connected
+            {
+                dpiX = 96.0 * presentationsource.CompositionTarget.TransformToDevice.M11;
+                dpiY = 96.0 * presentationsource.CompositionTarget.TransformToDevice.M22;
+            }
+            this.Left = SW / scalingFactor - this.Width;   //控件和屏幕分辨率值之间有差异
+            // this.Left = 1500;
             this.Top = 0;       
         }
 
@@ -38,6 +50,25 @@ namespace ControlClient
                 gameBar = new GameBar();
             }
             return gameBar;
+        }
+
+        [DllImport("gdi32.dll")]
+        static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+        public enum DeviceCap
+        {
+            VERTRES = 10,
+            DESKTOPVERTRES = 117,
+        }
+        private static float GetScalingFactor()
+        {
+            Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr desktop = g.GetHdc();
+            int logicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
+            int physicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
+
+            float screenScalingFactor = (float)physicalScreenHeight / (float)logicalScreenHeight;
+
+            return screenScalingFactor; // 1.25 = 125%
         }
     }
 }
